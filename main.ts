052e0814e599beff34a0d2a6ee4626eebb7649a3
Thabang Gideon Magaola <thabang@projectcodex.co>
2017-09-29 09:38:55 +0200

    modified existing code

---
+++ main.ts
@@ -0,0 +1,43 @@
+
+var user = <HTMLInputElement>document.getElementById('fname');
+
+var english = <HTMLInputElement>document.getElementById('english');
+var setswana = <HTMLInputElement>document.getElementById('setswana');
+var zulu = <HTMLInputElement>document.getElementById('zulu');
+var nameGreeted = {}
+function main() {
+
+    if (user.value !="") {
+        if(nameGreeted[user.value] == undefined){
+            nameGreeted[user.value] = 1;
+            greet();
+        }
+        if(nameGreeted[user.value] != undefined){
+            nameGreeted[user.value] += 1;
+            greet();
+        }
+    }
+    if (user.value == ""){
+        document.querySelector('.message').innerHTML = 'Please enter your name';
+    }
+    user.value = '';
+}
+
+function greet() {
+
+    if (english.checked) {
+        var greetMsg = "Hello, " + user.value + ".";
+    }
+    if (setswana.checked) {
+        var greetMsg = "Dumela, " + user.value + ".";
+    }
+    if (zulu.checked) {
+        var greetMsg = "Sawubona, " + user.value + ".";
+    }
+    if (greetMsg == undefined) {
+        document.querySelector('.message').innerHTML = 'Please select language of choice.'
+    }
+    if (greetMsg != undefined) {
+        document.querySelector('.message').innerHTML = greetMsg;
+    }
+}
